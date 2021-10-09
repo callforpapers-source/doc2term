7b5eb74360eea93a0c5d9a6ba7b79cc7918e2e20
callforpapers-source <65175289+callforpapers-source@users.noreply.github.com>
2021-10-09 12:08:18 -0400

    new version

--- doc2term/__init__.py
+++ doc2term/__init__.py
@@ -1,11 +1,8 @@
 from .doc2term import doc2term  # noqa
 from .doc2term import doc2term_str  # noqa
-from .doc2term import doc2term_list  # noqa
 
 __doc__ = "A fast NLP tokenizer that detects tokens and"\
 	" remove duplications and punctuations."\
 	" e.g doc2term.doc2term('input.txt', 'output.txt')."\
 	" doc2term_str(docs) gets a string and return the results"\
-	" in string format."\
-	" doc2term_list(docs) gets a list of documents and return"\
-	" results in string format."
+	" in string format."
